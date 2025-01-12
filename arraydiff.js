9d337c0c55100145e470e577b3b7d849d3d7600a
Liz Wade <11866103+lizwade@users.noreply.github.com>
2025-01-12 20:57:00 +0000

    add TODO action

--- arraydiff.js
+++ arraydiff.js
@@ -27,3 +27,63 @@ function arrayDiff(a, b) {
     return arrayDiff(filtered, b); /// AHA! I DID NOT HAVE THE WORD "RETURN" HERE. THIS HAS SOLVED EVERYTHING!!
   }
 }
+
+//below is a neater solution from other solvers of this kata.
+//I'm including it here so I can investigate relative perfomance.
+function array_diff_fav(a, b) {
+  return a.filter((e) => !b.includes(e));
+}
+
+//and here is my solution tidied up
+function arrayDiffLiz(a, b) {
+  if (b.length === 0) {
+    return a;
+  } else {
+    const toRemove = b.pop();
+    const filtered = a.filter((element) => element != toRemove);
+    return arrayDiffLiz(filtered, b);
+  }
+}
+
+//trying them both out
+let testValues = [];
+testValues = setTestValues();
+
+function setTestValues() {
+  return [
+    [
+      [2, 1, 6, 3],
+      [3, 1],
+    ],
+    [
+      [1, 2, 3, 4, 5, 6],
+      [5, 6, 7, 8, 9, 10],
+    ],
+  ];
+}
+
+// old version - we can't return the time taken when using console.time and console.timeEnd. Boo!
+function timeMyFunctionInTheConsole(functionToBeTimed) {
+  console.log(...testValues[0]); // display the arguments. remove this later
+  console.time(`Testing time taken by ${functionToBeTimed.name}`);
+  console.log(functionToBeTimed(...testValues[0])); // call function and display the result (to be sure of correct functioning while inside this test - remove display aspect later)
+  console.timeEnd(`Testing time taken by ${functionToBeTimed.name}`);
+}
+
+function timeMyFunction(functionToBeTimed) {
+  console.log(...testValues[0]); // display the arguments.remove this later
+  console.log(`Testing time taken by ${functionToBeTimed.name}`);
+  const start = performance.now();
+  console.log(functionToBeTimed(...testValues[0])); // call function and display the result (to be sure of correct functioning while inside this test - remove display aspect later)
+  const end = performance.now();
+  return end - start;
+}
+
+console.log("Doing timing tests.");
+console.log("First we'll time it with timeMyFunction:");
+testValues = setTestValues();
+const itTook = timeMyFunction(arrayDiffLiz);
+console.log(`itTook is ${itTook}`);
+console.log("Then we'll time it with timeMyFunctionInTheConsole:");
+testValues = setTestValues();
+timeMyFunctionInTheConsole(arrayDiffLiz);
